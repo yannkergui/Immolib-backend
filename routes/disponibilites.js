2fd5b8d0763256689a521b6341d127f56ec2310f
yann <yekerguillec@hotmail.fr>
2023-08-02 15:16:08 +0200

    routes dispo en cours

--- routes/disponibilites.js
+++ routes/disponibilites.js
@@ -7,19 +7,21 @@ const Disponibilites = require("../models/disponibilites");
 const { checkBody } = require("../modules/checkBody");
 
 //route pour créer une disponibilité
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
   // Vérifie que les champs ne sont pas vides
   if (!checkBody(req.body, ["dayOfWeek", "startTime", "endTime", "pro"])) {
     res.json({ result: false, error: "champs manquants ou vides" });
     return;
   }
+  const { dayOfWeek, startTime, endTime, pro} = req.body
       const newDisponibilite = new Disponibilites({
         dayOfWeek,
         startTime,
         endTime,
         pro,
       });
-      newDisponibilite.save().populate('pros')
+      newDisponibilite.save()
+      // .populate('pros')
       .then((newDoc) => {
         res.json({ result: true, data: newDoc });
       });
