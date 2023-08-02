3a2b186fa0df5b73957f1fd4b73042beadaf881b
yann <yekerguillec@hotmail.fr>
2023-08-02 15:11:04 +0200

    routes dispo en cours

--- routes/disponibilites.js
+++ routes/disponibilites.js
@@ -7,17 +7,12 @@ const Disponibilites = require("../models/disponibilites");
 const { checkBody } = require("../modules/checkBody");
 
 //route pour créer une disponibilité
-router.post("/:id", async (req, res) => {
+router.post("/", async (req, res) => {
   // Vérifie que les champs ne sont pas vides
   if (!checkBody(req.body, ["dayOfWeek", "startTime", "endTime", "pro"])) {
     res.json({ result: false, error: "champs manquants ou vides" });
     return;
   }
-  Disponibilites.findById(req.params.id).then((data) => {
-    // Vérifie si l'utilisateur n'est pas déjà enregistré dans la BDD
-    if (data === null) {
-      const { dayOfWeek, startTime, endTime, pro } = req.body;
-
       const newDisponibilite = new Disponibilites({
         dayOfWeek,
         startTime,
@@ -28,12 +23,8 @@ router.post("/:id", async (req, res) => {
       .then((newDoc) => {
         res.json({ result: true, data: newDoc });
       });
-    } else {
-      // Disponibilité déjà existante dans la BDD
-      res.json({ result: false, error: "Disponibilité déjà existante" });
-    }
   });
-});
+
 
 
 
